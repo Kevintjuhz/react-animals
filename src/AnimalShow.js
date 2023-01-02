e59f289dff9192aaed200440814b887f74ec8d85
kevinquaedvlieg <kevin@kqcreations.com>
2023-01-02 17:35:03 +0100

    Added images for the animals, and liking of the animals

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,8 +1,35 @@
+import {useState} from "react";
+import bird from "./svg/bird.svg"
+import cat from "./svg/cat.svg"
+import cow from "./svg/cow.svg"
+import dog from "./svg/dog.svg"
+import gator from "./svg/gator.svg"
+import heart from "./svg/heart.svg"
+
+const animalMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator
+}
 
 function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0)
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
     return (
-        <div>
-            {type}
+        <div onClick={handleClick}>
+            <img src={animalMap[type]} alt={"animal of type:" + type}/>
+            <img
+                src={heart}
+                alt="heart"
+                style={{
+                    width: 10 + 10 * clicks + "px",
+                }}
+            />
         </div>
     )
 }
